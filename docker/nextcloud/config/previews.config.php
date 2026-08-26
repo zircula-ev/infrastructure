5c960b686ca16df3ff4751476f641386dcd26cb8
IndieStu <98215909+IndieStu@users.noreply.github.com>
2026-08-26 13:00:26 +0200

    feat(nextcloud): configure image preview providers

---
+++ docker/nextcloud/config/previews.config.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$CONFIG = [
+    'enable_previews' => true,
+    'enabledPreviewProviders' => [
+        'OC\Preview\JPEG',
+        'OC\Preview\PNG',
+        'OC\Preview\GIF',
+        'OC\Preview\BMP',
+        'OC\Preview\XBitmap',
+        'OC\Preview\WebP',
+    ],
+];
